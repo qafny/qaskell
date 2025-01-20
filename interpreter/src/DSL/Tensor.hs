9665d26139682fcde98f66cc4a342c03d4d6fe34
liyili2 <liyili2@illinois.edu>
2025-01-19 21:12:56 -0600

    update

--- interpreter/src/DSL/Tensor.hs
+++ interpreter/src/DSL/Tensor.hs
@@ -36,15 +36,21 @@ instance Nesting a => Nesting (Tensor a) where
 -- Anni is annihilation, Dag is a dagger of a MuQ term, Tens is a tensor, sum is a linear sum, and circ is the sequencing operation
 -- data SndQ a b = I | Anni a b | Dag (SndQ a b) | Tens (SndQ a b) (SndQ a b) | Sum (SndQ a b) (SndQ a b) | Circ (SndQ a b) (SndQ a b)
 
-data Spin x = KeepPos x | KeepNeg x | Swap [x]
+-- generate choices, m is a list (superpositions), of t b, t is a data-structure. 
+-- For quantum, a data-structure is a basis-ket.
+-- consider the Univ data-structure, t need to be Univ s t a, where a = () OR Univ s t () OR Univ s t (Univ s t ())
+type ChoiceStrategy m t a b = t a -> m (t b)
 
-data Univ s a = Univ {
+generateChoices :: (Monad m, Traversable t) =>
+                   ChoiceStrategy m t a b -> t a -> m (t b)
+
+--possible example, gen_superpositions
+                   
+-- comonad data-structure 
+data Univ s t (a:: t -> Type) = Univ {
   sextract :: s -> a,
-  sval :: [s]
+  sval :: t s --how to encode this.
 }
-
-instance Foldable (Univ s a) where
-  foldM f (Univ xtract v) = Univ xtract (foldr f v)
 
 instance Functor (Univ s) where
    fmap f (Univ xtract v) = Univ xtract (fmap f v)
@@ -58,44 +64,69 @@ instance Extend (Univ s) where
 instance Comonad (Univ s) where
    extract :: Univ s a -> a
    extract (Univ xtract v) = xtract v
-   
--- (>>=) :: Univ s a -> (Univ s a -> b) -> Univ s b
---   store@(Univ xtract v) >>= f = 
- --      let b = f store
---           xtract' s = f (Univ xtract s) 
---       in Univ b xtract'
+
+                   
+data Spin = KeepPos | KeepNeg | ToPos | ToNeg
+
+--traverse on m level, (a -> m b) -> t a -> m (t b) , where b = [Spin], each spin sequantially applies to a data-point
+firstSwap q = traverse (\x -> [ToPos, ToNeg]) q
+
+firstSwapM m = foldr (\q -> traverse (\x -> [ToPos, ToNeg]) q) m [] -- t b
+
+applyTwo :: t (t a) -> b -> b -> t (t b)
+applyTwo (Univ xtr v) x y = Univ (fmap (\q => let Univ xtr' v' = (xtr q) in Univ xtr' (fmap (\p -> y) v')) v) (fmap (\q -> x) v)
+
+applyThree :: t (t (t a)) -> b -> b -> b -> t (t (t b))
+applyThree (Univ xtr v) x y z = Univ (fmap (\q => let Univ xtr' v' = (xtr q) 
+           in Univ xtr' (fmap (\p => let Univ xtr'' v'' = (xtr' p) in Univ xtr'' (fmap (\r -> z) v'')) v'')) (fmap (\q -> y) v')) (fmap (\q -> x) v)
+
+
+secondSwap = traverse (\x -> [applyTwo (duplicate x) ToPos ToNeg] ++ [applyTwo (duplicate x) ToNeg ToPos]) q
+
+secondSwapM = foldr (\q -> traverse (\x -> [applyTwo (duplicate x) ToPos ToNeg] ++ [applyTwo (duplicate x) ToNeg ToPos]) q) m []
+
        
 --define examples, Univ s (), returns Univ ([Spin s]) () , 
 -- Clique Finding, 
 -- Gen x. Gen y. KeepPos x . KeepPos y
 -- Gen x. Gen y. Swap [x,y]
-let dd = duplicate d in let sd = sval dd in sd <&> (\x -> (((sextract dd) x) <&> (\y -> [KeepPos x, KeepPos y])))
-
-let dd = duplicate d in let sd = sval dd in sd <&> (\x -> (((sextract dd) x) <&> (\y -> [Swap [x,y]])))
+bindChoice1 m = let dd = duplicate m in applyTwo dd KeepPos KeepPos
+bindChoice2 m = secondSwapM m
 
 --Equal Sum, 
 -- Gen x. Swap [x]
 -- ID
-let sd = sval d in sd <&> (\x -> duplicate [Swap [x]])
+bindChoice1 m = firstSwapM m
 
---ID is implemented as 
-
-let dd = duplicate d in let sd = sval dd in sd <&> (\x -> [KeepPos x]) ++ sd <&> (\x -> [KeepNeg x])
+bindChoice2 m = m <&> (\q -> applyTwo (duplicate q) KeepPos KeepNeg
 
 --Exact Cover
-let sd = sval d in sd <&> (\x -> duplicate [Swap [x]])
+bindChoice1 m = firstSwapM m
+--let sd = sval d in sd <&> (\x -> duplicate [Swap [x]])
+bindChoice2 m = m <&> 
+        (applyThree (duplicate duplicate d) KeepPos KeepNeg KeepNeg)
+        ++ (applyThree (duplicate duplicate d) KeepNeg KeepPos KeepNeg)
+                ++ (applyThree (duplicate duplicate d) KeepNeg KeepNeg KeepPos)
 
-let ddd = duplicate duplicate d in let sd = sval dd in
-     sd <&> (\x -> (((sextract dd) x) <&> (\y -> ((sextract dd) x) <&> (\z -> append [KeepPos x, KeepNeg y, KeepNeg z] 
-                       (append [KeepNeg x, KeepPos y, KeepNeg z] [KeepNeg x, KeepNeg y, KeepPos z])))))
-                 
+flatternOne :: t b -> [b]
+flatternOne (Univ f v) = v
+flatternOneM m = m <&>  flatternOne
+
+flatternTwo :: t t b -> [b]
+flatternTwo (Univ f v) = foldr (\q -> foldr (\p m -> [q,p]++m) (f q) []) v []
+flatternTwoM m = m <&>  flatternTwo
+
+flatternThree :: t t t b -> [b]
+flatternThree (Univ f v) = foldr (\q -> foldr (\Univ f' v' -> \ m -> (foldr (\r m -> [q,p,r]++m) (f q) [])) (f q) []) v []
+flatternThreeM m = m <&>  flatternThree
+
+
 -- Amp adding steps
 
 -- Clique Finding, 
 --AppAmp (KeepPos x . KeepPos y) = if g(x,y) then (* 0) else ID -- load the edge weight between x and y
 --Forall (*) AppAmp 1. -- the equal sum example has a sum of all AppAmp
-appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 1
-  where aux [KeepPos x, KeepPos y] = if g(x,y) then (*0) else (*1)
+appAmp m = foldr (\q -> (foldr (\p -> if p == [KeepPos, KeepPos] && g(x,y) then (*0) else (*1)) (flatternTwoM q) 1, q)) m (1,[])
 
 --equal sum example, just access the x-th element.
 -- In (+) operation forall case, ID means (+0)
@@ -103,9 +134,12 @@ appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 1
 --AppAmp (KeepNeg x) = - g(x)
 --Forall (+) AppAmp 0. -- the equal sum example has a sum of all AppAmp
 
-appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 0
-  where aux [KeepPos x] = (+ (g(x))) 
-        aux [KeepNeg x] = (+ (- g(x)))
+appAmp m = foldr (\q -> foldr (\p -> if p == [KeepPos] then (+ (g(x))) else (+ (- g(x)))) (flatternOneM q) 0, q) m (1,[])
+
+-- flatternOneM q <&> if q == [KeepPos x] then (+ (g(x))) else (+0)) m []
+-- foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 0
+--  where aux [KeepPos x] = (+ (g(x))) 
+--        aux [KeepNeg x] = (+ (- g(x)))
   
   
 -- Exact Cover, 
@@ -114,10 +148,18 @@ appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 0
 --AppAmp (KeepNeg x. KeepNeg y. KeepPos z) = if h(x,y,z) then (* 0) else ID
 
 --Forall (*) AppAmp 1. -- the equal sum example has a sum of all AppAmp
-appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 1
-  where aux [KeepPos x, KeepNeg y, KeepNeg z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
-        aux [KeepNeg x, KeepPos y, KeepNeg z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
-        aux [KeepNeg x, KeepNeg y, KeepPos z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
+appAmp m = foldr (\q -> (foldr (\p -> if p == [KeepPos, KeepNeg,KeepNeg] && h(1,0,0) then (*0)
+                                      else if p == [KeepNeg,KeepPos,KeepNeg] && h(0,1,0) then (*0)
+                                      else if p == [KeepNeg,KeepNeg,KeepPos] && h(0,0,1) then (*0) else (*1)) (flatternTwoM q) 1, q)) m (1,[])
+
+--appAmp dd = foldM (dd <&> (\x -> ((sextract dd) x) <&> (\y -> (aux y, y)))) dd 1
+--  where aux [KeepPos x, KeepNeg y, KeepNeg z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
+--        aux [KeepNeg x, KeepPos y, KeepNeg z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
+--        aux [KeepNeg x, KeepNeg y, KeepPos z] = if h(g(x),g(y),g(z)) then (* 0) else (*1)
+
+solveF :: (Foldable f, Ord a) =>
+  f a -> a
+solveF = minimum
 
 
 -- Selection step:  Sel = Gen x . Sel: for each data-structure, we create an x to represent a site.
