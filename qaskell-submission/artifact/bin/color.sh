4ddbb026da1871577a91d651a77b302cdabc81a3
James Bryan Graves <jbg@Macintosh-2.local>
2025-06-05 16:33:44 +0200

    add color to artifacts

--- qaskell-submission/artifact/bin/color.sh
+++ qaskell-submission/artifact/bin/color.sh
@@ -3,7 +3,7 @@
 target="$1"
 
 echo "Graph Color Classical:"
-cabal run qaskell -- eqsum classical
+cabal run qaskell -- color classical
 
 echo "Graph Color Quantum:"
 cabal run qaskell -- color quantum | python3 scripts/${target}/parser.py > scripts/${target}/color.py && python3 scripts/${target}/color.py
--- qaskell-submission/artifact/run.sh
+++ qaskell-submission/artifact/run.sh
@@ -3,5 +3,6 @@
 docker build -t qaskell .
 docker run --rm qaskell /qaskell/qaskell-submission/qaskell/eqsum.sh "qiskit"
 docker run --rm qaskell /qaskell/qaskell-submission/qaskell/eqsum.sh "dwave"
+docker run --rm qaskell /qaskell/qaskell-submission/qaskell/color.sh "qiskit"
 docker run --rm qaskell /qaskell/qaskell-submission/qaskell/clique.sh "qiskit"
 docker run --rm qaskell /qaskell/qaskell-submission/qaskell/infer.sh "qiskit"
