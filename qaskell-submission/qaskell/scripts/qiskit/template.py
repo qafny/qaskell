a0fd788decf7ed6862814351c7add7a7e481fc48
zx <szx917@gmail.com>
2026-04-06 14:37:22 -0400

    updated python parser and template, changed how gate parameters are calculated, fixed bugs that angles are scaled with time interpolation

--- qaskell-submission/qaskell/scripts/qiskit/template.py
+++ qaskell-submission/qaskell/scripts/qiskit/template.py
@@ -1,4 +1,4 @@
-# template.py
+# template.py - Adiabatic Quantum Evolution
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit.circuit.library import RZGate, RZZGate, RXXGate, RYYGate
 from qiskit.providers.basic_provider import BasicSimulator
@@ -6,35 +6,28 @@ from qiskit.visualization import plot_histogram
 import numpy as np
 import matplotlib.pyplot as plt
 
-gamma = 0.5
-beta  = 0.1
 n_qubits = 3
-
-trotter_steps = 10
-T = 1.0
+T = 100.0  # Total evolution time
+trotter_steps = 500  # Number of Trotter steps
+dt = T / trotter_steps
 
 qr = QuantumRegister(n_qubits)
 cr = ClassicalRegister(n_qubits, name='cr')
 qc = QuantumCircuit(qr, cr)
 
-is_clique = False
 
-if is_clique:
-    for step in range(trotter_steps):
-        for j in range(n_qubits):
-            for k in range(n_qubits):
-                if j != k:
-                    qc.append(RXXGate(4 * gamma / trotter_steps), [j, k])
-                    qc.append(RYYGate(4 * gamma / trotter_steps), [j, k])
-else:
-    for i in range(n_qubits):
-        qc.h(i)
+for i in range(n_qubits):
+    qc.h(i)
 
 for step in range(trotter_steps):
-# INSERT_RZZ_GATES_HERE
+    t = step / trotter_steps
+    gamma = t
+    beta = 1-t
 
+# INSERT_RZZ_GATES_HERE
+    
     for i in range(n_qubits):
-        qc.rx(2 * beta, i)
+        qc.rx(2 * beta * dt, i)
 
 qc.measure(range(n_qubits), range(n_qubits))
 
@@ -45,4 +38,13 @@ job = simulator.run(compiled_circuit, shots=1024)
 result = job.result()
 counts = result.get_counts()
 
-print(result.get_counts())
+# Sort by count and show top results
+sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+
+top5 = sorted(counts.items(), key=lambda x: x[1], reverse=True)[:5]
+print("Top 5 counts:")
+print(top5)
+
+# histogram
+print("Plotting histogram")
+plot_histogram(counts)
